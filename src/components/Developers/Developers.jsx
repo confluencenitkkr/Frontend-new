0becf2bed095724100d8672386abb8ea74fc792d
Inzamam <inzamamchowdhury20@gmail.com>
2022-04-20 16:22:54 +0530

    learn

--- src/components/Developers/Developers.jsx
+++ src/components/Developers/Developers.jsx
@@ -23,7 +23,7 @@ const Developers = () => {
     {
       "name":"Inzamam",
       "image" : "",
-      "role": "",
+      "role": "Front-end",
       "insta": "",
       "linkedin": "",
       "github":"",
