600699eb9f5b6fb3a8c75586f9aef62341265e76
Nihar Meena <109016776+niharx@users.noreply.github.com>
2022-10-13 00:02:30 +0530

    Changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from "react";
 import "../../styles/Home.css";
 
 const Home = () => {
-
+  //   useEffect(()=>{
+  // var vid = document.getElementById("myVideo");
+  // vid.autoplay = true;
+  // vid.load();
+  //   },[])
   return (
     <>
       <main className="home_main_main">
-      {/* <img src="/images/bgMedia/circle.png" alt="" id="main_container_cirle" className="home_circle_img_pro"/> */}
+        {/* <img src="/images/bgMedia/circle.png" alt="" id="main_container_cirle" className="home_circle_img_pro"/> */}
         <div id="text">
           <h1>
             <div className="first_home">A</div>
@@ -16,6 +20,9 @@ const Home = () => {
             <div className="third_home">Soiree</div>
           </h1>
         </div>
+        {/* <audio autoPlay={1}>
+          <source src="/images/BgMusic.mp3" type="audio/ogg" />
+        </audio> */}
       </main>
     </>
   );
