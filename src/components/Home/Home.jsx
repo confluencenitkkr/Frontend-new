fac2c89e4e865112f26279fe6885ccae83bad7bc
Kshitiz Pratap Singh <kshitiz100mit@gmail.com>
2022-10-12 19:50:08 +0530

    homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from "react";
 import "../../styles/Home.css";
 
 const Home = () => {
-
   return (
     <>
       <main className="home_main_main">
-      {/* <img src="/images/bgMedia/circle.png" alt="" id="main_container_cirle" className="home_circle_img_pro"/> */}
+        <img
+          src="/images/bgMedia/circle.png"
+          alt=""
+          id="main_container_cirle"
+          className="home_circle_img_pro"
+        />
         <div id="text">
           <h1>
             <div className="first_home">A</div>
@@ -15,6 +19,11 @@ const Home = () => {
             </div>
             <div className="third_home">Soiree</div>
           </h1>
+        </div>
+
+        <div className="radio_wrapper_home">
+          <img src="/images/bgMedia/radio.png" alt="" className="radio_radio"/>
+          <img src="/images/bgMedia/table.png" alt="" className="radio_table"/>
         </div>
       </main>
     </>
