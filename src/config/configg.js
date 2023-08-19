a35c946f3f503214ae65aa497cff25a237cf2b74
karan <karan999867@gmail.com>
2023-08-19 13:41:11 +0530

    update

--- src/config/configg.js
+++ src/config/configg.js
@@ -2,7 +2,7 @@ const settings = {
 
     // BACKEND_URL: 'http://localhost:4044',
 
-    BACKEND_URL: 'https://nitkkr.herokuapp.com'
+    BACKEND_URL: 'https://confluence-backend.onrender.com'
 
 }
 
